0499d32e268fa804d734e03a92fc923fe784c263
Ala Kiani <alakianii@gmail.com>
2017-11-26 15:47:36 +0330

    Add Handlebars

--- server/app.js
+++ server/app.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
+//Handlebar Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+//How middleware works
+app.use((req, res, next) => {
+  req.startTime = Date.now();
+  next();
+});
+
+//GET '/'
+app.get('/', (req, res) => {
+  res.send('Salam');
+  req.time = Date.now() - req.startTime;
+  console.log(`${req.method} ${req.originalUrl} ${req.time}(ms)`);
+});
 
 const port = 5000;
 
